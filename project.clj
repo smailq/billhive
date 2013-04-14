b8d4a86a81427759fd384b3a0d4f03de9f92f27c
Kyu Lee <smailq@gmail.com>
2013-04-14 12:21:13 -0400

    Compojure stuff

--- project.clj
+++ project.clj
@@ -1,7 +1,12 @@
+
 (defproject billhive "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :main billhive.core)
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [compojure "1.1.5"]]
+  :plugins [[lein-ring "0.8.2"]]
+  :ring {:handler billhive.handler/app}
+  :profiles
+  {:dev {:dependencies [[ring-mock "0.1.3"]]}})
+
+
