d58de8b0c8994e11c85cd907bb76ca516adf8c39
Kyu Lee <smailq@gmail.com>
2013-04-14 23:12:09 -0400

    Transferring layout/design

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject billhive "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [compojure "1.1.5"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [compojure "1.1.5"]
+                 [de.ubercode.clostache/clostache "1.3.1"]]
   :plugins [[lein-ring "0.8.2"]]
   :ring {:handler billhive.handler/app}
   :profiles
--- src/billhive/handler.clj
+++ src/billhive/handler.clj
@@ -1,10 +1,23 @@
 (ns billhive.handler
-  (:use compojure.core)
+  (:use compojure.core clostache.parser)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]))
 
+
+(defn show-landing-page [req] ;; ordinary clojure function, accepts a request map, returns a response map
+  ;; return landing page's html string. possible template library:
+  ;; mustache (https://github.com/shenfeng/mustache.clj, https://github.com/fhd/clostache...)
+  ;; enlive (https://github.com/cgrand/enlive)
+  ;; hiccup(https://github.com/weavejester/hiccup)
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    (render (slurp "templates/landing.html") {
+                      :total_due "1230"})
+  })
+
+
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] show-landing-page)
   (route/resources "/")
   (route/not-found "Not Found"))
 
